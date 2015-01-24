c49c41a87ecb0b862f524807a59f5e3c2403b7d7
jaewo7o <jaewo7o@naver.com>
2015-01-24 23:34:55 +0900

    자바패턴 초기로딩

---
+++ src/main/java/com/jaewoo/pattern/iterator/ChannelIterator.java
@@ -0,0 +1,6 @@
+package com.jaewoo.pattern.iterator;
+
+public interface ChannelIterator {
+	boolean hasNext();
+    Channel next();
+}
---
+++ src/main/java/com/jaewoo/pattern/iterator/ChannelTypeEnum.java
@@ -0,0 +1,5 @@
+package com.jaewoo.pattern.iterator;
+
+public enum ChannelTypeEnum {
+	ENGLISH, HINDI, FRENCH, ALL;
+}
