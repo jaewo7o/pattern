437588f3440fab672992acd731670bb4790542f7
jaewo7o <jaewo7o@naver.com>
2016-02-11 22:23:01 +0900

    decorator pattern

---
+++ src/main/java/com/jaewoo/pattern/decorator/HouseBlend.java
@@ -0,0 +1,11 @@
+package com.jaewoo.pattern.decorator;
+
+public class HouseBlend extends Beverage {
+	public HouseBlend() {
+		description = "House Blend Coffee";
+	}
+
+	public double cost() {
+		return .89;
+	}
+}
